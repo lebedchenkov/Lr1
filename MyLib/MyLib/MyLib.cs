f94649b04537de0ad2458e6352c43de6b60aee50
lebedchenkov <81518728+lebedchenkov@users.noreply.github.com>
2021-10-25 18:41:26 +0300

    Lab1

---
+++ MyLib/MyLib/MyLib.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib1
+    /// <summary>
+    /// Calculating mathematical functions
+    /// </summary>
+{
+    public class MyLib
+    {
+        /// <summary>
+        /// Ploshchad
+        /// </summary>
+        public static double Ploshchad(double x1, double x2, double x3, double y1, double y2, double y3)
+        {
+            double AB, BC, AC, S, p;
+            AB = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            BC = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
+            AC = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2));
+            p = (AB + BC + AC) / 2;
+            if (AB + BC > AC && BC + AC > AB && AC + AB > BC)
+            {
+                S = Math.Sqrt(p * (p - AB) * (p - BC) * (p - AC));
+                return S;
+            }
+            else
+            {
+                S = 0;
+                return S;
+            }
+        }
+        /// <summary>
+        /// Summa_row
+        /// </summary>
+        public static double Summa_row(double x, int n)
+        {
+            double S;
+            S = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                S = Math.Pow((Math.Sin(x)), n) + S;
+            }
+            return S;
+        }
+        /// <summary>
+        /// Factorial
+        /// </summary>
+        public static double Factorial(int n)
+        {
+            return n * (n - 1);
+        }
+        /// <summary>
+        /// Summa_row2
+        /// </summary>
+        public static double Summa_row2(int n, double x)
+        {
+            double S = 1;
+            for (int i = 0; i < n; i++)
+            {
+                double k = Factorial(n);
+                S = S + (((n * n + 1) / k) * (Math.Pow((x / 2), n)));
+            }
+            return S;
+        }
+        /// <summary>
+        /// Error
+        /// </summary>
+        public static double Error(int n, double x)
+        {
+            double Sum = Summa_row2(n, x);
+            double p = ((x * (x / 4) + (x / 2) + x) * Math.Pow(Math.E, (x / 2)));
+            double t = ((Math.Abs(Sum - p) / p) * 100);
+            return t;
+        }
+    }
+} 
---
+++ MyLib/Mybibl/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib1;
+
+namespace Mybibl
+{
+    /// <summary>
+    /// program for checking the use of the library MyLib
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// calling library methods from MyLib.dll
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            double x1, x2, x3, y1, y2, y3, x, S;
+            int n;
+            Console.WriteLine("Задание1:");
+            Console.WriteLine("Введите координаты первой вершины:");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты второй вершины:");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты третьей вершины:");
+            x3 = Convert.ToDouble(Console.ReadLine());
+            y3 = Convert.ToDouble(Console.ReadLine());
+            // calling the calculation method "Ploshchad" from the library
+            S = MyLib.Ploshchad(x1, x2, x3, y1, y2, y3);
+            Console.WriteLine("Площадь треугольника = " + S + " кв.ед.");
+            Console.WriteLine("Задание2:"); 
+            Console.WriteLine("Введите число:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите натуральное число рядов:");
+            n = Convert.ToInt32(Console.ReadLine());
+            // calling the calculation method "Summa_row" from the library
+            S = Math.Round(MyLib.Summa_row(x, n), 3);
+            Console.WriteLine("Сумма ряда = " + S);
+            Console.WriteLine("Задание3:");
+            Console.WriteLine("Введите число:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите натуральное число рядов:");
+            n = Convert.ToInt32(Console.ReadLine());
+            // calling the calculation method "Error" from the library
+            S = Math.Round(MyLib.Error(n, x), 3);
+            Console.WriteLine("Погрешность = " + S + " %");
+            Console.ReadKey();
+        }
+    }
+}
